29c176d013d2236073f9aea946ce234a4a53a4b4
shubhamdasgupta <shubhamdasgupta2003@gmail.com>
2023-08-26 22:23:10 +0530

    Added javascript for location detection button

--- Ambulance Service/location.js
+++ Ambulance Service/location.js
@@ -21,3 +21,56 @@ function closePopup()
 {
     locationWin.style.display = 'none';
 }
+
+// ================== Reverse Geocoding using Pincode =========================
+
+const rev_btn = document.getElementById('pin-apply');
+let pincode = document.getElementById('zipcode');
+const loc_txt = document.getElementById('location-txt');
+console.log(pincode);
+
+api_key = "ee2dfca941774c139225977bbddebb90" 
+
+rev_btn.addEventListener("click", ()=>{
+    let pincd = pincode.value;
+    fetch('https://api.opencagedata.com/geocode/v1/json?q='+pincd+'&key='+api_key)
+    .then(response=>(response.json()))
+    .then(result=>{
+        let details = result.results[0];
+        // console.log(details['formatted']);
+        loc_txt.innerHTML = details['formatted'];
+        console.log(details.components);
+    })
+
+})
+
+// ================== Geocoding using Lat & Lon =========================
+
+const btn_loc = document.getElementById('det-location');
+
+function onSuccess(position)
+{
+    let {latitude,longitude} = position.coords;
+    console.log(latitude,longitude);
+    fetch('https://api.opencagedata.com/geocode/v1/json?q='+latitude+','+longitude+'&key='+api_key)
+    .then(response=>(response.json())).then(result=>{
+        let details = result.results[0];
+        loc_txt.innerHTML = details['formatted'];
+    })
+}
+
+function onError(error)
+{
+    console.log(error);
+}
+
+btn_loc.addEventListener("click", ()=>{
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(onSuccess,onError);
+    }
+    else
+    {
+        btn_loc.innerText = "Geoloaction not supported";
+    }
+})
