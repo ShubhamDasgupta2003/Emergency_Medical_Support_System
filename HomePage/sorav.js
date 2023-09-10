f69efb6b3b0180dd59364dd709a4a85cbffc3a66
Jbiebes <jagannathsarkar212@gmail.com>
2023-09-10 18:55:48 +0530

    modify home page

---
+++ HomePage/sorav.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Hospital.', 'medicine store.', 'Blood bank.', 'Ambulance.', 'medical technician.'],
+    typeSpeed: 120,
+    loop: true,
+    loopCount: Infinity,
+});
