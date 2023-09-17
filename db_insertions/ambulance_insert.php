02ee846ff4cec685d877ffae9d7b0b6656dfb13b
shubhamdasgupta <shubhamdasgupta2003@gmail.com>
2023-09-17 14:13:06 +0530

    Added two new queries to ambulance_insertion php file

--- db_insertions/ambulance_insert.php
+++ db_insertions/ambulance_insert.php
@@ -8,9 +8,12 @@
 
     INSERT IGNORE INTO `ambulance_info`(`amb_no`, `amb_name`, `amb_type`, `amb_status`, `amb_loc_lat`, `amb_loc_long`, `amb_rate`, `amb_contact`, `amb_driver_name`, `amb_state`, `amb_district`, `amb_town`, `amb_loc_pincode`) VALUES ('UP35B3695','Atal Bihari Ambulance Service','Life-Support','busy','23.5312','86.3644','1000','9654123666','Vijay','Uttarpradesh','Prayagraj','George Town','211001');
     
-    INSERT IGNORE INTO `ambulance_info`(`amb_no`, `amb_name`, `amb_type`, `amb_status`, `amb_loc_lat`, `amb_loc_long`, `amb_rate`, `amb_contact`, `amb_driver_name`, `amb_state`, `amb_district`, `amb_town`, `amb_loc_pincode`) VALUES ('WB24B2100','Sastha Sundar Ambulance Service','Normal','active','21.3145','87.9524','549','9748329717','Suman','West Bengal','Hooghly','Chinsura','743123');";
-
+    INSERT IGNORE INTO `ambulance_info`(`amb_no`, `amb_name`, `amb_type`, `amb_status`, `amb_loc_lat`, `amb_loc_long`, `amb_rate`, `amb_contact`, `amb_driver_name`, `amb_state`, `amb_district`, `amb_town`, `amb_loc_pincode`) VALUES ('WB24B2100','Sastha Sundar Ambulance Service','Normal','active','21.3145','87.9524','549','9748329717','Suman','West Bengal','Hooghly','Chinsura','743123');
     
+    UPDATE `ambulance_info` SET `amb_no` = 'WB24B3446' WHERE `ambulance_info`.`amb_no` = '0';
+    
+    ALTER TABLE user_info ADD COLUMN formatted_adrrs varchar(255);";
+
     $result = mysqli_multi_query($con,$query);
 
     if($result)
