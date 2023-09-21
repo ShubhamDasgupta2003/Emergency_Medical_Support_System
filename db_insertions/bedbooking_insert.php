af105d639080a9c56ac8ae93c227a04f092e9160
SouravPaul2002 <sourav97972@gmail.com>
2023-09-21 17:35:38 +0530

    minor changes

--- db_insertions/bedbooking_insert.php
+++ db_insertions/bedbooking_insert.php
@@ -2,25 +2,8 @@
     //Path for main config file 
     include_once("db_config/main_config.php");
     
-    //This php file inserts data into ambulance_info table in mysql
-
-
     
 
-    // $query = "ALTER TABLE `hospital_info` ADD `Address` VARCHAR(100) NOT NULL AFTER `ContactNo`;
-    
-    // ALTER TABLE `hospital_info` CHANGE `ContactNo` `ContactNo` BIGINT(10) NOT NULL;
-
-    // ALTER TABLE `hospital_info` CHANGE `Address` `Address` VARCHAR(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;";
-
-    $query = "ALTER TABLE `patient_booking_info` DROP `Booking_time`;
-
-    ALTER TABLE `patient_booking_info` ADD `Dob` DATE NOT NULL AFTER `Gender`, ADD `Address` VARCHAR(200) NOT NULL AFTER `Dob`, ADD `City` VARCHAR(50) NOT NULL AFTER `Address`, ADD `Pin` INT(10) NOT NULL AFTER `City`;
-
-    
-    ALTER TABLE `patient_booking_info` CHANGE `Booking_date` `Booking_date` VARCHAR(255) NOT NULL;
-    ALTER TABLE `patient_booking_info` CHANGE `Patient_id` `Patient_id` VARCHAR(20) NOT NULL;
-    ALTER TABLE `patient_booking_info` ADD `Hospital_name` VARCHAR(100) NOT NULL AFTER `Booking_amount`";
 
     $query = "INSERT IGNORE INTO `hospital_info`(`Id`, `Name`, `ContactNo`, `Address`, `State`, `District`, `City`, `Pincode`, `Latitude`, `Longitude`, `Male_bed_available`, `Female_bed_available`, `Bed_charge`) VALUES ('1','Naihati Matri Sadan Municipal Hospital','6384479131','6,Rishi Bankim Chandra Road,Naihati Urban,Naihati,West Bengal 743165','West Bengal','North 24 Parganas','Naihati','743165','22.8885','88.4178','52','48','300');
 
