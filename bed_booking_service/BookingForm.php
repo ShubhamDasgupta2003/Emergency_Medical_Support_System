e446c0179dccb6558164a412845fdaf6eab46839
SouravPaul2002 <sourav97972@gmail.com>
2023-09-26 12:37:20 +0530

    mail sending working properly

--- bed_booking_service/BookingForm.php
+++ bed_booking_service/BookingForm.php
@@ -27,6 +27,7 @@ if(isset($_POST['submit'])){
     $age=$_POST['age'];
     $contact=$_POST['contact'];
     $dob=$_POST['dob'];
+    $p_email=$_POST['email'];
     $address2=$_POST['address2'];
     $city=$_POST['city'];
     $pin=$_POST['pin'];
@@ -91,7 +92,7 @@ if(isset($_POST['submit'])){
     //'storing hospitaal name in patient table accordingly' php code ends here
 
 
-    $sql2="INSERT INTO `patient_booking_info` (Hospital_name,Patient_id,Patient_name,Gender,Age,ContactNo,Dob,address2,City,Pin,Booking_date) VALUES ('$row[Name]','$patient_id','$name','$gender','$age','$contact','$dob','$address2','$city','$pin','$bookdatetime')";
+    $sql2="INSERT INTO `patient_booking_info` (Hospital_name,Patient_id,Patient_name,Gender,Age,ContactNo,Dob,email,address2,City,Pin,Booking_date) VALUES ('$row[Name]','$patient_id','$name','$gender','$age','$contact','$dob','$email','$address2','$city','$pin','$bookdatetime')";
     $result=mysqli_query($conn,$sql2);
 
 
@@ -110,17 +111,27 @@ if(isset($_POST['submit'])){
 
     <!-- for send mail to hospital for notify them that a patient booked a bed  -->
 
+<!-- 
+    $p_id=$_SESSION['p_id'];
 
-    <!-- if(isset($_POST['submit'])){
+    $p_mail_query = "SELECT * FROM `patient_booking_info` WHERE Patient_id = '$p_id'";
+    $result=mysqli_query($conn,$p_mail_query);
+    if($result){
+        $p_row=mysqli_fetch_assoc($result);
+    }else{
+        echo "error";
+    }
+    if(isset($_POST['submit'])){
 
-        // $mailid = $_GET['emailid'];
-        // $recp_name = $_GET['name'];
+        $email = $p_row['email'];
+        $recp_name = $p_row['name'];
         // $userid = $_GET['userid'];
+        $contact = $p_row['ContactNo'];
 
-        $to_email = "sourav97972@gmail.com";//specific hospital email should be here
-        $subject = "Bed booked by a patient";
-        $body = "a bed is booked for patient.Here is the patient details
-        Name: $name
+        $to_email = "$email";
+        $subject = "Bed booked successfully";
+        $body = "Your bed booking is done. Here are the details
+        Patient name: $recp_name
         Contact number : $contact";
 
         $headers = "From: emergencymedicalservices23@gmail.com";//this email was created for this project
@@ -131,6 +142,64 @@ if(isset($_POST['submit'])){
             echo "Email failed";
         }
     } -->
+
+<?php
+
+        $id=$_GET['hospitalid'];
+        $sql= "SELECT * FROM `hospital_info` where Id=$id";
+        $result= mysqli_query($conn,$sql);
+        $row=mysqli_fetch_assoc($result);
+
+
+
+
+if(isset($_POST['submit'])){
+
+    //patient details vriables starts here
+        $email = $p_email;
+        $recp_name = $name;
+        // $userid = $_GET['userid'];
+        $p_contact = $contact;
+        $p_id= $patient_id;
+    //patient details vriables ends here
+
+    //hospital details vriables starts here
+        $hosp_name= "$row[Name]";
+        $hosp_contact= "$row[ContactNo]";
+        $hosp_address= "$row[Address]";
+
+    //hospital details vriables ends here
+
+        $to_email = "$email";
+        $subject = "Confirmation and Information for Your Hospital Bed Reservation";
+        $body = "Dear $recp_name,
+
+------------------------------------------------------------------------------------------------------------------
+
+We are writing to confirm and provide important information regarding youreservation for a hospital bed at $hosp_name . We appreciate your trust in ouSwasth Sampark services and look forward to assisting you in future.
+
+
+Patient details:
+
+Patient name: $recp_name
+Patient id: $p_id
+
+Hospital details:
+
+Name: $hosp_name
+Contact number: $hosp_contact
+Address: $hosp_address";
+
+        $headers = "From: emergencymedicalservices23@gmail.com";//this email was created for this project
+
+        if (mail($to_email, $subject, $body, $headers)) {
+            echo "Email sent ";
+        } else {
+            echo "Email failed";
+        }
+}
+?>
+
 
         <!-- mail code ends here  -->
 
@@ -216,6 +285,10 @@ if(isset($_POST['submit'])){
                             </div>
                         </div>
                     </div>
+                    <div class="input-box" id="age">
+                            <label>Email address</label>
+                            <input type="email" name="email" placeholder="Enter email address" required />
+                        </div>
                     <div class="input-box" id="age">
                             <label>Aadhaar card</label>
                             <input type="number" name="adhr_num" placeholder="Enter aadhaar card number" required />
