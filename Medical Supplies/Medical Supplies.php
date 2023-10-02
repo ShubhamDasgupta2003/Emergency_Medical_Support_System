f33cf010d7c996d4ff065b653a8d5c6316f266b7
Aatraya23 <royaatraya7@gmail.com>
2023-10-02 21:00:03 +0530

    location

--- Medical Supplies/Medical Supplies.php
+++ Medical Supplies/Medical Supplies.php
@@ -1,5 +1,49 @@
 <?php
 include_once ('connection.php');
+
+
+//Backend for location modification starts here
+setcookie("loc_modify","false");
+
+$uid =  $_SESSION['user_id'];
+$ufname =  $_SESSION['user_fname'];
+$ulname = $_SESSION['user_lname'];
+
+$lat_in_use = 0.0;
+$lon_in_use = 0.0;
+$full_address = "";
+$loc_query = "SELECT lat_in_use,long_in_use,formatted_adrrs FROM user_info WHERE user_id='$uid'";
+
+$loc_result = mysqli_query($con,$loc_query);
+$loc_rows = $loc_result->fetch_assoc();
+
+if($loc_result)
+{
+    $lat_in_use = $loc_rows['lat_in_use'];
+    $lon_in_use = $loc_rows['long_in_use'];
+    $full_address = $loc_rows['formatted_adrrs'];
+}
+else
+{
+    echo "error";
+}
+
+if($_COOKIE['loc_modify'] == 'true')
+{
+    $mod_lat = $_COOKIE['lat_in_use'];
+    $mod_lon = $_COOKIE['lon_in_use'];
+    $mod_addrs = $_COOKIE['address_in_use'];
+
+    $loc_mod_query = "UPDATE user_info SET lat_in_use=$mod_lat,long_in_use=$mod_lon,formatted_adrrs='$mod_addrs' WHERE user_id='$uid'";
+
+    $mod_loc_result = mysqli_query($con,$loc_mod_query);
+
+    if($mod_loc_result)
+    {
+        header("Refresh: 1");
+    }
+}
+//Backend for location modification ends here
 ?>
 
 <!DOCTYPE html>
@@ -113,30 +157,36 @@ include_once ('connection.php');
         </div>
 
 
-<div class="location-window" id="loc-win">
-    <div class="card popup">
-        <button class="dismiss-btn" id="dismiss">&times</button>
-        <div class="loc-head">
-            <span>Enter an Indian pincode here</span>
-            <div class="loc-option-tab">
-                <input type="number" name="pincode" placeholder="Pincode here" id="zipcode">
-                <button class="btn" id="pin-apply">Apply</button>
+  <!-- Location window popup starts here -->
+            
+  <div class="location-window" id="loc-win">
+                <div class="card popup">
+                    <button class="dismiss-btn" id="dismiss">&times</button>
+                    <div class="loc-head">
+                        <span>Enter an Indian pincode here</span>
+                        <div class="loc-option-tab">
+                            <input type="number" name="pincode" placeholder="Pincode here" id="zipcode">
+                            <button class="btn" id="pin-apply">Apply</button>
+                        </div>
+                    </div>
+                    <div class="loc-head">
+                        <span>Allow to access your location</span>
+                        <div class="loc-option-tab">
+                            <button class="get-location btn" id="det-location"><i class="fa-solid fa-location-crosshairs"></i>Detect my location</button>
+                        </div>
+                    </div>
+                    <div class="loc-head">
+                        <div class="loc-option-tab">
+                            <label for="" id="location-txt">
+                                <?php
+                                    echo "$full_address";
+                                ?>
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="loc-head">
-            <span>Allow to access your location</span>
-            <div class="loc-option-tab">
-                <button class="get-location btn" id="det-location"><i class="fa-solid fa-location-crosshairs"></i>Detect my location</button>
-            </div>
-        </div>
-        <div class="loc-head">
-            <div class="loc-option-tab">
-                <label for="" id="location-txt"></label>
-            </div>
-        </div>
-    </div>
-</div>
-
+            <!-- Location window popup ends here -->
 
 
 <footer>
