3f9eab946aabb36b0c94d5af07d39e4c64737f0b
shubhamdasgupta <shubhamdasgupta2003@gmail.com>
2023-10-08 13:42:50 +0530

    Created the payment interface

---
+++ Payment/payment_ackn.php
@@ -0,0 +1,9 @@
+<?php
+
+    session_start();
+
+    $user_id = $_SESSION['user_id'];
+
+
+    echo "Thank you for payment";
+?>
---
+++ Payment/payment_process.php
@@ -0,0 +1,22 @@
+<?php
+
+    session_start();
+
+    date_default_timezone_set("Asia/calcutta");
+    $user_id = $_SESSION['user_id'];
+    $order_id = $GET['order_id'];
+    $cur_date = date("Y-m-d");
+    $cur_time = date("H:i:s");
+
+    include "db_config/main_config.php";
+
+    if(isset($_POST['amount']) && isset($_POST['payment_id']))
+    {
+        $pid = $_POST['payment_id'];
+        $amount = $_POST['amount'];
+
+        $query = "INSERT INTO `payment`(`payment_id`, `order_id`, `user_id`, `payment_type`, `time`, `date`, `amount`, `payment_status`) VALUES ('$pid','$order_id','$user_id','Online','$cur_time','$cur_date','$amount','Complete')";
+
+        $result = mysqli_query($con,$query);
+    }
+?>
