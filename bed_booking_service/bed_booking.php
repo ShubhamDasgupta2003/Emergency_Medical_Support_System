ccbcf860dce3d0d53225195592d4db5232e1ff35
SouravPaul2002 <sourav97972@gmail.com>
2023-10-09 19:25:33 +0530

    user avatar added in bbs

--- bed_booking_service/bed_booking.php
+++ bed_booking_service/bed_booking.php
@@ -102,7 +102,16 @@
             <a class="navlink" href="/Minor Project 5th_Sem/Emergency_Medical_Support_System/HomePage/#footer">Contact Us</a>
         </nav>
         
-        <a href="#" id="user-avatar"><i class="fa-solid fa-user fa-lg account-avatar"></i></a>
+        <!-- <a href="#" id="user-avatar"><i class="fa-solid fa-user fa-lg account-avatar"></i></a> -->
+        <div class="user-avatar-container">
+        <a href="http://localhost/Minor%20Project%205th_Sem/Emergency_Medical_Support_System/HomePage/profile.php" id="user-avatar"><i class="fa-solid fa-user fa-lg account-avatar"></i></a>
+            <?php
+                
+                echo"<h3>$_SESSION[user_fname]</h3>";
+            ?>
+
+        </div>
+        <div id="menu-btn" class="fa fa-bars"> </div>
 
         <div id="menu-btn" class="fa fa-bars"> </div>
     </header>
