2ec910665e965a983767f34ac4932057e14648d5
shubhamdasgupta <shubhamdasgupta2003@gmail.com>
2023-10-13 20:30:13 +0530

    Added the backend for location pop up in home page

--- HomePage/js/location.js
+++ HomePage/js/location.js
@@ -27,7 +27,7 @@ function openPopup()
     }
     else
     {
-        alert("Please Login!");
+        alert("Please Login to get services nearest to you!");
     }
 }
 
@@ -45,7 +45,7 @@ let pincode = document.getElementById('zipcode');
 const loc_txt = document.getElementById('location-txt');
 console.log(pincode);
 
-api_key = "ee2dfca941774c139225977bbddebb90" 
+api_key = "ee2dfca941774c139225977bbddebb90";
 
 rev_btn.addEventListener("click", ()=>{
     let pincd = pincode.value;
@@ -53,12 +53,20 @@ rev_btn.addEventListener("click", ()=>{
     .then(response=>(response.json()))
     .then(result=>{
         let details = result.results[0];
-        // console.log(details['formatted']);
-        loc_txt.innerHTML = details['formatted'];
-        console.log(details.components);
-    })
+        // console.log(result.results);
+        let loc_txt = details['formatted'];
+        let loc_geometry = details['geometry'];
+        let latitude = loc_geometry['lat'];
+        let longitude = loc_geometry['lng'];
+        // console.log(latitude,longitude);
 
-})
+        document.cookie = "lat_in_use= "+latitude;
+        document.cookie = "lon_in_use= "+longitude;
+        document.cookie = "address_in_use= "+loc_txt;
+        document.cookie = "loc_modify= "+true;
+        window.location.reload();
+    })
+}) 
 
 // ================== Geocoding using Lat & Lon =========================
 
@@ -68,10 +76,15 @@ function onSuccess(position)
 {
     let {latitude,longitude} = position.coords;
     console.log(latitude,longitude);
+    document.cookie = "lat_in_use= "+latitude;
+    document.cookie = "lon_in_use= "+longitude;
     fetch('https://api.opencagedata.com/geocode/v1/json?q='+latitude+','+longitude+'&key='+api_key)
     .then(response=>(response.json())).then(result=>{
         let details = result.results[0];
-        loc_txt.innerHTML = details['formatted'];
+        let loc_txt = details['formatted'];
+        document.cookie = "address_in_use= "+loc_txt;
+        document.cookie = "loc_modify= "+true;
+        window.location.reload();
     })
 }
 
