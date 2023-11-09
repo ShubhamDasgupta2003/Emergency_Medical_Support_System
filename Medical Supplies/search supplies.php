777b31aef0272dfdfdbd5a9cd1493c06ba767901
Aatraya23 <royaatraya7@gmail.com>
2023-11-10 01:42:30 +0530

    changes

--- Medical Supplies/search supplies.php
+++ Medical Supplies/search supplies.php
@@ -1,12 +1,12 @@
 <?php
 include_once ('oop_connectionp.php');
 include_once ('location.php');
-$obj=new Database;
 session_start();
+$obj=new Database;
 
+$s=0;
 
 $uid =  $_SESSION['user_id'];
-
 $islogin =  $_SESSION['is_logged_in'];
 if($islogin!=1)
 {
@@ -22,10 +22,9 @@ $ufname =  $_SESSION['user_fname'];
 $ulname = $_SESSION['user_lname'];
 
 
-
 //-------------------search-----------------------------------
 
-$amb_filter_query = "active";
+$amb_filter_query = "a";
 
 if(@$_GET['q'])
 {
@@ -36,13 +35,23 @@ else
     $amb_filter_query = "active";
 }
 // $search_filter = ;
-$sqli_table = 'medical_supplies_technical';
+$sqli_table = 'medical_supplies_medical';
 $sqli_rows = "`product_id`, `source_id`, `product_name`, `product_rate`, `product_image`, `product_para`, `product_desc`, `product_makers`, `password`, `email`, `phone`";
 
 $sqli_condition = "product_name LIKE '$amb_filter_query%' OR product_rate='$amb_filter_query' OR product_para='$amb_filter_query' OR product_desc= '$amb_filter_query'";
 $sqli_order = 'product_id';
 
 //---------------------------------------------------------------
+
+if(isset($_GET['search_data_product']))
+{
+    $search_data_value=$_GET['search_data'];
+    $search_data_value= trim($search_data_value);
+    $search_query="SELECT * FROM medical_supplies_medical WHERE product_keywords like '%$search_data_value%'";
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -76,19 +85,21 @@ $sqli_order = 'product_id';
  <!-- header section start -->
  <header class="header">
     <a href="#" class="logo"><i class="fa-solid fa-heart-pulse"></i>medcare</a>
-    <form action="search_tech supplies.php" method="get">
+    <form action="" method="get">
     <div class="search-bar" id="srchbar-above">
-        <button class="get-location btn" id="get-location-btn" style="width:50px;"><i class="fas fa-map-marker-alt"></i></button>
+        <button class="get-location btn" id="get-location-btn" style="width:50px;"><i class="fas fa-map-marker-alt"></i></button>       
         <input type="text" placeholder="Search" name=search_data>
-       <button class="btn" value="submit" name="search_data_product"><i class="fa-solid fa-magnifying-glass"></i></button>  
+       <button class="btn" value="submit" name="search_data_product"><i class="fa-solid fa-magnifying-glass"></i></button> 
+       
     </div>
-</form>
+    </form> 
     <nav class="navbar">
         <a class="navlink" href="/Minor Project 5th_Sem/Emergency_Medical_Support_System/HomePage/index.php">Home</a>
             <a class="navlink" href="/Minor Project 5th_Sem/Emergency_Medical_Support_System/HomePage/index.php#services">Services</a>
             <a class="navlink" href="/Minor Project 5th_Sem/Emergency_Medical_Support_System/HomePage/index.php#review">Review</a>
             <a class="navlink" href="/Minor Project 5th_Sem/Emergency_Medical_Support_System/HomePage/index.php#footer">contact Us</a>
     </nav>
+    
     
     <div class="user-avatar-container">
         <a href="http://localhost/Minor%20Project%205th_Sem/Emergency_Medical_Support_System/HomePage/profile.php" id="user-avatar"><i class="fa-solid fa-user fa-lg account-avatar"></i></a>
@@ -99,14 +110,16 @@ $sqli_order = 'product_id';
 
         </div>
         <div id="menu-btn" class="fa fa-bars"> </div>
-   
 
 
+
+   <div id="cart">
     <?php
      $n=$obj->numrecord($uid);
-     ?>
-     <div id="cart"> <a href="cart.php" id="user-cart"><i class="fa badge fa-cart-shopping fa-2xl" value=<?php echo $n ?> style="color:black"></i></a> </div>
-   
+    
+     ?> 
+     <a href="cart.php" id="user-cart"><i class="fa badge fa-cart-shopping fa-2xl" value="<?php echo $n ?>" style="color:black"></i></a> 
+   </div>
 </header>
 
 <!-- header section end -->
@@ -120,14 +133,14 @@ $sqli_order = 'product_id';
     
 <div class="hero-section">
     <div class="content">  
-        <p class="sub-heading">Technical Supplies at your Doorstep</p>
+        <p class="sub-heading">Medical Supplies at your Doorstep</p>
     </div>
 </div>
 
 <div class="secondnav">
     <nav class="segmented-navigation">
-        <a href="Medical Supplies.php" class="segmented-item">Medical Supplies</a>
-        <a href="Technical Supplies.php" class="segmented-item active">Technical Supplies</a>
+        <a href="Medical Supplies.php" class="segmented-item active">Medical Supplies</a>
+        <a href="Technical Supplies.php" class="segmented-item ">Technical Supplies</a>
       </nav>
     </div>
 
@@ -136,9 +149,10 @@ $sqli_order = 'product_id';
 
             <!-- Your content goes here | check body_cont.css file for css property-->
 
-         <div class="cards">
+     <div class="cards">
           <?php
-            $records=$obj->viewrecordt();
+            $records=$obj->viewsearch( $search_query);
+         
             foreach($records as $row)
             {
          
@@ -150,12 +164,13 @@ $sqli_order = 'product_id';
                     <p class='card-address'>..... </p>
                     <p class='card-type'> $row[product_para]</p>
                     <p class='card-fare'>&#8377 $row[product_rate]</p>
-                    <a href='Detailed Supply.php?pid=$row[product_id]&name=technical'><button class='btn btn-secondary-orange'>Details</button></a>
+                    <a href='Detailed Supply.php?pid=$row[product_id]&name=medical'><button class='btn btn-secondary-orange'>Details</button></a>
                 </div>
             </div>";
             
+            $s=$s+1;
             }
-         
+          
 
         ?>
             </div>
@@ -240,5 +255,6 @@ $sqli_order = 'product_id';
         <!-- Footer bar -->
     <script src="location.js"></script>   
     <script src="common.js"></script>
+    <script src="search.js"></script>
 </body>
 </html>
--- Medical Supplies/test.php
+++ Medical Supplies/test.php
@@ -4,12 +4,14 @@ $obj=new Database;
 session_start();
 $uid='USR8882889123';
 $n=$obj->numrecord($uid);
+/*
 echo $n;
 $current_date=date('Y-m-d');
 $current_time=date('h:i:sa');
-/*
-$row=$obj->viewrecorddt("1");
 
+$records=$obj->viewrecordm();
+foreach($records as $row)
+{
 
 
 echo" <div class='card'>
@@ -22,8 +24,9 @@ echo" <div class='card'>
         <a href='Detailed Supply.php?pid=$row[product_id]&name=medical'><button class='btn btn-secondary-orange'>Details</button></a>
     </div>
 </div>";
-*/
 
+}
+/*
 $product_name="test";
 $product_price=5;
 $product_image="testimg";
@@ -31,4 +34,31 @@ $product_quantity=7;
 $s=$obj->insertcart($product_name,$product_price,$product_image, $product_quantity,$uid);
 $ufname="test";
 $l=$obj->insertallorder($uid,$ufname,$current_date,$current_time);
+*/
+if(isset($_GET['search_data_product']))
+{
+    $search_data_value=$_GET['search_data'];
+    $search_data_value= trim($search_data_value);
+    $search_query="SELECT * FROM medical_supplies_medical WHERE product_keywords like '%$search_data_value%'";
+    echo $search_query;
+}
+$records=$obj->viewsearch($search_query);
+foreach($records as $row)
+{
+
+
+echo" <div class='card'>
+    <img src='$row[product_image]' >
+    <div class='card-details'>
+        <p class='card-name'>$row[product_name]</p>
+        <p class='card-address'>..... </p>
+        <p class='card-type'> $row[product_para]</p>
+        <p class='card-fare'>&#8377 $row[product_rate]</p>
+        <a href='Detailed Supply.php?pid=$row[product_id]&name=medical'><button class='btn btn-secondary-orange'>Details</button></a>
+    </div>
+</div>";
+
+}
+
+
 ?>
